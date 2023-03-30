facab172d6e2da5c4fc286167d766b65b7eab3ab
Sebastián López <sebaslopez.2002@gmail.com>
2023-03-30 18:12:44 -0500

    Analytics of vercel added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Poppins } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 
 const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
@@ -11,6 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={poppins.className}>
       <Component {...pageProps} />
+      <Analytics />
     </main>
   );
 }
