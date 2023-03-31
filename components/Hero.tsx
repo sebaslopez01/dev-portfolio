a1f8d70e7ff73518a31d03f184972668da650f3f
Sebastián López <sebaslopez.2002@gmail.com>
2023-03-30 22:57:01 -0500

    t

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -24,6 +24,7 @@ export default function Hero() {
         </div>
       </div>
       {/* <PugCanvas /> */}
+      {/* Hola */}
       <div className="absolute xs:-bottom-20 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
