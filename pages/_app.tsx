0e1e457b19e733888d7a3676444fe93a9db3e319
Sebastián López <sebaslopez.2002@gmail.com>
2023-03-30 20:37:19 -0500

    google Analytics added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Poppins } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
+import Head from "next/head";
+import Script from "next/script";
 
 const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
@@ -10,9 +12,22 @@ const poppins = Poppins({
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={poppins.className}>
-      <Component {...pageProps} />
-      <Analytics />
-    </main>
+    <>
+      <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-7235SXXVY4" />
+      <Script id="google-analytics">
+        {`window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-7235SXXVY4');`}
+      </Script>
+      <main className={poppins.className}>
+        <Component {...pageProps} />
+        <Analytics />
+      </main>
+    </>
   );
 }
